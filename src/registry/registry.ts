a42c73881025eb4f2d584ff419e046d368d7d7a7
kanyadirichard <100612707+kanyadirichard@users.noreply.github.com>
2024-03-12 17:03:29 +0100

    Update registry.ts
    add getPrivateKey

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -1,8 +1,9 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { REGISTRY_PORT } from "../config";
+import { generateKeyPair } from "./cryptoFunctions";
 
-export type Node = { nodeId: number; pubKey: string };
+export type Node = { nodeId: number; pubKey: string; privateKey: string };
 
 export type RegisterNodeBody = {
   nodeId: number;
@@ -31,9 +32,23 @@ export async function launchRegistry() {
       return res.status(400).json({ error: `Node ${nodeId} is already registered.` });
     }
 
-    registeredNodes.push({ nodeId, pubKey });
+    const { privateKey, publicKey } = generateKeyPair();
+    registeredNodes.push({ nodeId, pubKey, privateKey });
 
     return res.status(200).json({ message: `Node ${nodeId} registered successfully.` });
+  });
+
+  registry.get("/getPrivateKey/:nodeId", (req: Request, res: Response) => {
+    const nodeId: number = parseInt(req.params.nodeId);
+
+    // Find the node by ID
+    const node = registeredNodes.find((n) => n.nodeId === nodeId);
+    if (!node) {
+      return res.status(404).json({ error: "Node not found." });
+    }
+
+    // Respond with the private key (base64 encoded)
+    return res.status(200).json({ result: node.privateKey });
   });
   
   _registry.get("/status", (req: Request, res: Response) => {
